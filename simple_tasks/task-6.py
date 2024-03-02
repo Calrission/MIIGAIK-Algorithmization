a240b04a1482006a44250fc12f9a34a50eca075b
artemii <kavrese@gmail.com>
2024-03-02 11:03:44 +0300

    complete task-6.py

---
+++ simple_tasks/task-6.py
@@ -0,0 +1,6 @@
+data = input().split(" ")
+for i in range(len(data)):
+    if (i+1) % 2 != 0:
+        print(data[i+1] if len(data)-1 > i else data[i], end=" ")
+    else:
+        print(data[i-1], end=" ")
